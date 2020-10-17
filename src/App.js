b79f8eb7d321c5041bb63114a43a3cb60910a889
VigneshOfficial2020 <vigneshofficial2020@gmail.com>
2020-10-17 12:08:45 +0530

    BRANCH f1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 function App() {
-  const h1 = <h2>Hello world</h2>;
+  const h1 = <h1>Hello world</h1>;
+  const f1 = <h1>BRANCH f1</h1>;
   return (
     <div className="vicky">
      {h1}
+     {f1}
     </div>
   );
 }
